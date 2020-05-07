cd8d64bdbaf5ce5a208f1eb600a4ab6fff5d9d4f
Michelle Poole <michelle.poole.email@gmail.com>
2020-05-07 12:49:42 -0700

    successfully grabbing student details from form

--- components/app.js
+++ components/app.js
@@ -2,10 +2,13 @@ class App {
     constructor(gradeTable, pageHeader, gradeForm) {
         this.gradeTable = gradeTable,
         this.pageHeader = pageHeader,
-        this.gradeForm = this.gradeForm,
+        this.gradeForm = gradeForm,
 
-        this.handleGetGradesError = this.handleGetGradesError.bind(this);
-        this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+        this.handleGetGradesError = this.handleGetGradesError.bind(this),
+        this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this),
+        this.createGrade = this.createGrade.bind(this),
+        this.handleCreateGradeError = this.handleCreateGradeError.bind(this),
+        this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this)
     }
 
     handleGetGradesError(error) {
@@ -45,8 +48,21 @@ class App {
         );
     }
 
+    createGrade(name, course, grade){
+        console.log(name, course, grade);
+    }
+
+    handleCreateGradeError(error){
+        console.error(error);
+    }
+
+    handleCreateGradeSuccess(){
+        this.getGrades();
+    }
+
     start(){
         this.getGrades();
+        this.gradeForm.onSubmit(this.createGrade);
     }
 
 }
--- components/grade-form.js
+++ components/grade-form.js
@@ -10,7 +10,17 @@ class GradeForm {
     }
 
     handleSubmit(event){
-        console.log('in handleSubmit');
+        //console.log('in handleSubmit', event.target);
         event.preventDefault();
+
+        let formData = new FormData(event.target);
+
+        const name = formData.get('name');
+        const course = formData.get('course');
+        const grade = formData.get('grade');
+
+        this.createGrade(name, course, grade);
+
+        event.target.reset();
     }
 }
